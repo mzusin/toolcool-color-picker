952f2c6c970827f1e65768393c065eaac2aa2b34
Tool Cool <toolcool.org@gmail.com>
2022-07-18 14:43:00 +0300

    update color picker version

--- run/settings.js
+++ run/settings.js
@@ -1,5 +1,16 @@
 import pcssPlugin from './esbuild-plugins/esbuild-pcss-plugin.js';
 import esbuildCopyPlugin from './esbuild-plugins/esbuild-copy-plugin.js';
+import fs from 'fs';
+import path from 'path';
+
+const packageJson = fs.readFileSync(path.join(process.cwd(), './package.json'), 'utf-8');
+let version = '1.0.1';
+
+try{
+    const parsed = JSON.parse(packageJson);
+    version = parsed.version;
+}
+catch(ex){}
 
 export default {
     entryPoints: ['./src/index.ts'],
@@ -17,6 +28,6 @@ export default {
     },
     plugins: [pcssPlugin, esbuildCopyPlugin],
     banner: {
-        js: '// Tool Cool Picker v1.0.1, MIT License - https://github.com/toolcool-org/toolcool-color-picker',
+        js: `// Tool Cool Picker v${ version }, MIT License - https://github.com/toolcool-org/toolcool-color-picker`,
     },
 }
